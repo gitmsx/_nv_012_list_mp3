9d93bfd54f9f47ec238dedbfdb8875c0b284e0a6
gitmsx <gitmsx@mail.ru>
2022-11-25 19:57:16 +0300

    Saved on 25.11.2022-19:57:15,87

--- Assets/Scripts/StartAudioScript.cs
+++ Assets/Scripts/StartAudioScript.cs
@@ -12,46 +12,37 @@ public class StartAudioScript : MonoBehaviour
     AudioSource audioSource;
     int CurrentClip = 0;
 
-    DateTime timeStart;
     DateTime timeEnd;
-
-
-
     string[] ListAllMp3;
-     [SerializeField] float TimeToChangeMp3 = 7;
 
-
-    [SerializeField] AudioClip flySound;
+    [SerializeField] float TimeToChangeMp3 = 7;
 
     void Start()
     {
         int CurrentClip = 0;
 
-        timeEnd = DateTime.Now;
+        //  timeEnd = DateTime.Now;
         audioSource = GetComponent<AudioSource>();
         AudioLibMp3 audioLibMp3 = new AudioLibMp3();
-        string PathToFolderMp3 = "I:\\data1\\mp3.eng\\HarryPotter";
+       // string PathToFolderMp3 = "I:\\data1\\mp3.eng\\HarryPotter";
+        string PathToFolderMp3 = "U:\\_cs5\\No Copyright Library";
+
+
         ListAllMp3 = audioLibMp3.Mp3FilesToArray(PathToFolderMp3);
 
         StartCoroutine(ListPlay());
-
-
-
     }
 
 
 
     IEnumerator ListPlay()
     {
-
-
         double dellta = (timeEnd - DateTime.Now).TotalSeconds;
         Debug.Log(" ListPlay Seconds " + dellta.ToString() + " timeEnd > DateTime.Now " + (timeEnd > DateTime.Now).ToString());
 
         while (timeEnd > DateTime.Now)
             yield return new WaitForSeconds((float)dellta);
         StartCoroutine(LoadAudioClip(ListAllMp3[CurrentClip++]));
-
     }
 
 
@@ -59,18 +50,18 @@ public class StartAudioScript : MonoBehaviour
 
     IEnumerator LoadAudioClip(string PtsPath)
     {
-
         using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip((PtsPath), AudioType.MPEG))
         {
             yield return uwr.SendWebRequest();
             _audioClip = DownloadHandlerAudioClip.GetContent(uwr);
             audioSource.clip = _audioClip;
             audioSource.Play();
-            timeStart = DateTime.Now;
-            timeEnd = timeStart.AddSeconds(_audioClip.length);
-            if (TimeToChangeMp3>0)  // fast change for debug and etc
-            timeEnd = timeStart.AddSeconds(TimeToChangeMp3);
+
+            timeEnd = DateTime.Now.AddSeconds(_audioClip.length);
+            if (TimeToChangeMp3 > 0)  // fast change for debug and etc 
+                timeEnd = DateTime.Now.AddSeconds(TimeToChangeMp3);
             Debug.Log("TotalSeconds to change track = " + (timeEnd - DateTime.Now).TotalSeconds);
+
             StartCoroutine(ListPlay());
 
         }
